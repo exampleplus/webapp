55f67a4fc7459a407e2286806e7d364db2d66f7a
修改index.添加底部导航 <384868059@qq.com>
2018-01-10 19:14:27 +0800

    feat(静态页面完成，模拟假数据后端接口)

---
+++ js/sendReuest.js
@@ -0,0 +1,14 @@
+		mui.init({
+			swipeBack:true //启用右滑关闭功能
+		});
+		var $grid=$("#grid");
+		var $li=$grid.find("li");
+		var $span=$li.find('span:first')
+		var mui_bg_minpin=$(".mui-bg-minpin");
+		var mui_bg_fangchan=$(".mui-bg-fangchan");
+		var mui_bg_qiche=$(".mui-bg-qiche");
+		var mui_bg_qita=$(".mui-bg-qita");
+		
+		$("#sendOnline").click(function(){
+			window.location.href="saleOnline.html"
+		})
